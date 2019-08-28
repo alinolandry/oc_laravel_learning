8d9b95ad6c12e0d2d613a08d60d0370faf3043b0
Alain Noutchomwo <alinolandry@gmail.com>
2019-08-28 16:04:47 +0100

    initialiszing git porject and starting 2nd part of OC tutorial

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\PostRepository;
+use App\Http\Requests\PostRequest;
+
+class PostController extends Controller
+{
+
+    protected $postRepository;
+
+    protected $nbrPerPage = 4;
+
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->middleware('auth', ['except' => 'index']);
+        $this->middleware('admin', ['only' => 'destroy']);
+
+        $this->postRepository = $postRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = $this->postRepository->getPaginate($this->nbrPerPage);
+        $links = $posts->render();
+
+        return view('posts.liste', compact('posts', 'links'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        $inputs = array_merge($request->all(), ['user_id' => $request->user()->id]);
+
+        $this->postRepository->store($inputs);
+
+        return redirect(route('post.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->postRepository->destroy($id);
+
+        return redirect()->back();
+    }
+}
---
+++ resources/views/posts/liste.blade.php
@@ -0,0 +1,44 @@
+@extends('template')
+
+@section('header')
+	@if(Auth::check())
+		<div class="btn-group pull-right">
+			{!! link_to_route('post.create', 'Créer un article', [], ['class' => 'btn btn-info']) !!}
+            {!! Form::open(['method' => 'POST', 'route' => ['logout']]) !!}
+                {!! Form::submit('Deconnexion', ['class' => 'btn btn-warning ']) !!}
+            {!! Form::close() !!}
+		</div>
+	@else
+		{!! link_to('login', 'Se connecter', ['class' => 'btn btn-info pull-right']) !!}
+	@endif
+@endsection
+
+@section('contenu')
+	@if(isset($info))
+		<div class="row alert alert-info">{{ $info }}</div>
+	@endif
+	{!! $links !!}
+	@foreach($posts as $post)
+		<article class="row bg-primary">
+			<div class="col-md-12">
+				<header>
+					<h1>{{ $post->titre }}</h1>
+				</header>
+				<hr>
+				<section>
+					<p>{{ $post->contenu }}</p>
+					@if(Auth::check() and Auth::user()->admin)
+						{!! Form::open(['method' => 'DELETE', 'route' => ['post.destroy', $post->id]]) !!}
+							{!! Form::submit('Supprimer cet article', ['class' => 'btn btn-danger btn-xs ', 'onclick' => 'return confirm(\'Vraiment supprimer cet article ?\')']) !!}
+						{!! Form::close() !!}
+					@endif
+					<em class="pull-right">
+						<span class="glyphicon glyphicon-pencil"></span> {{ $post->user->name }} le {!! $post->created_at->format('d-m-Y') !!}
+					</em>
+				</section>
+			</div>
+		</article>
+		<br>
+	@endforeach
+	{!! $links !!}
+@endsection
